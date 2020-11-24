48fae9ff7f3a32f776d10e8ebed2094c6dd16e0a
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-24 18:21:39 +0545

    chore(App.js): addition of states
    Added states and fetched books from API along with book shelf changer method

--- src/App.js
+++ src/App.js
@@ -16,10 +16,31 @@ class BooksApp extends React.Component {
          * users can use the browser's back and forward buttons to navigate between
          * pages, as well as provide a good URL they can bookmark and share.
          */
-        showSearchPage: false
+        showSearchPage: false,
+        books: [],
+        shelves: [],
     }
 
+    shelves = [];
+
+    componentDidMount() {
+        BooksAPI.getAll().then(books => {
+            books.map(book => (!this.shelves.includes(book.shelf.trim())) && this.shelves.push(book.shelf));
+            this.setState({books, shelves: this.shelves});
+        });
+    }
+
+    changeShelf = (e, book) => {
+        const books = [...this.state.books];
+        const sameShelfBooks = books.filter(b => b.id !== book.id);
+        const changedShelfBook = books.filter(b => b.id === book.id)[0];
+        changedShelfBook.shelf = e.currentTarget.value;
+        const updatedBooks = [...sameShelfBooks, changedShelfBook];
+        BooksAPI.update(book, e.currentTarget.value).then(() => this.setState({books: updatedBooks}))
+    };
+
     render() {
+        const {books, shelves} = this.state;
         return (
             <div className="app">
                 {this.state.showSearchPage ? (
@@ -27,7 +48,7 @@ class BooksApp extends React.Component {
                 ) : (
                     <div className="list-books">
                         <Header/>
-                        <BookList/>
+                        <BookList books={books} shelves={shelves} onChange={this.changeShelf}/>
                         <div className="open-search">
                             <button onClick={() => this.setState({showSearchPage: true})}>Add a book</button>
                         </div>
