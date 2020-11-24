36a5a8f7dc9ea86c21634e844629bb6260c3d24f
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-24 14:56:25 +0545

    feat(components): added a book detail component
    Extracted the book title and book author elements to a new component

---
+++ src/components/BookDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const BookDetail = () => {
+    return (
+        <React.Fragment>
+            <div className="book-title">To Kill a Mockingbird</div>
+            <div className="book-authors">Harper Lee</div>
+        </React.Fragment>
+    );
+};
+
+export default BookDetail;
