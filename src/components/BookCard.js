f1bdd24b2120a03073fc6370c83613af22e14ac9
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-24 18:15:02 +0545

    chore(components): passed props to BookCard
    Passing essential props to BookCard component

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -3,11 +3,11 @@ import BookTop from "./BookTop";
 import BookDetail from "./BookDetail";
 
 
-const BookCard = () => {
+const BookCard = ({book, shelves, onChange}) => {
     return (
         <div className="book">
-            <BookTop/>
-            <BookDetail/>
+            <BookTop book={book} shelves={shelves} onChange={onChange}/>
+            <BookDetail title={book.title} authors={book.authors}/>
         </div>
     );
 };
