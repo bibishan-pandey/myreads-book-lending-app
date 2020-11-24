97b631b2cc36ab6feb3af742d25d4df3a9471a8a
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-24 18:20:00 +0545

    chore(components): passed data as props
    Filtered books by shelves as per the fetched books and its consecutive shelves

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,13 +2,19 @@ import React from 'react';
 import BookShelf from "./BookShelf";
 
 
-const BookList = () => {
+const BookList = ({books, shelves, onChange}) => {
     return (
         <div className="list-books-content">
             <div>
-                <BookShelf/>
-                <BookShelf/>
-                <BookShelf/>
+                {shelves.map((shelf, index) => {
+                    const title = shelf === 'currentlyReading'
+                        ? 'Currently Reading' : shelf === 'wantToRead'
+                            ? 'Want to Read' : "Read";
+                    const shelfBooks = books.filter(book => book.shelf === shelf);
+                    return <BookShelf
+                        key={index}
+                        title={title} books={shelfBooks} shelves={shelves} onChange={onChange}/>;
+                })}
             </div>
         </div>
     );
