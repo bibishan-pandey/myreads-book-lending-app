76c731390cca99d1586639fc327e0afd300de7bd
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-25 11:46:55 +0545

    Updates

---
+++ precache-manifest.fd21d8a04d32849b39f1d11a4ba83872.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "67d35a03638eb68771cc",
+    "url": "/myreads-book-lending-app/static/css/main.1cab0b16.chunk.css"
+  },
+  {
+    "revision": "67d35a03638eb68771cc",
+    "url": "/myreads-book-lending-app/static/js/main.67d35a03.chunk.js"
+  },
+  {
+    "revision": "0772abc16085c236ee61",
+    "url": "/myreads-book-lending-app/static/js/1.0772abc1.chunk.js"
+  },
+  {
+    "revision": "5f14187e0a74715dfa2b",
+    "url": "/myreads-book-lending-app/static/js/runtime~main.5f14187e.js"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-book-lending-app/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-book-lending-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-book-lending-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "9cd919a75d5359b589892943f43eebb0",
+    "url": "/myreads-book-lending-app/index.html"
+  }
+];
